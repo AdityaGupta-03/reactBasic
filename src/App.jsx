c86072dc8ee835cd648d14d790e52e733acd5d5b
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-16 21:57:08 +0530

    Using name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //  To style the app.jsx: App.css is used
 import './App.css';
 
+let name = "Coffee with Addy";
 function App() {
   // Only 1 Tag is returned by the React
   return (
@@ -8,7 +9,7 @@ function App() {
     <>
       <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">Navbar</a>
+          <a className="navbar-brand" href="/">{name}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
