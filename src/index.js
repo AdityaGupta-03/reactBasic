f2de8a15feeb4a6aec2b8c380ef2017ea37b6f5d
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-16 21:45:01 +0530

    Commenting index.js file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//  This File uses index.css for styling the index.html
+// Entry point in the index.html file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Uses Strict Mode : Used to catch the unsafe coding mistakes and throws the error
   <React.StrictMode>
+    {/* Calling the App Component i.e App.jsx */}
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
