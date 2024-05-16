410838fa3d33d7f1892504cb54d63091c6eab9c4
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-17 00:28:05 +0530

    creating navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,28 @@
 //  To style the app.jsx: App.css is used
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
-let name = "Coffee with Addy";
 let loggedIn = true;
-function Success(){
-  return(
-    <button className="btn btn-danger">Logout</button>
-  );
+function Success() {
+    return <button className="btn btn-danger">Logout</button>;
 }
-function Danger(){
-  return(
-    <button className="btn btn-success">Login</button>
-  );
+function Danger() {
+    return <button className="btn btn-success">Login</button>;
 }
-let content = loggedIn ? <Success /> : <Danger />
+let content = loggedIn ? <Success /> : <Danger />;
 
 // Exporting the Component
 export default function App() {
-  // Only 1 Tag is returned by the React
-  return (
-    // This is the react Fragment to return multiple things as a single container
-    <>
-      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">{name}</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true" href='/'>Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-
-      {/* Nesting Function in the App Component */}
-      <div className="container">
-        <h1>Hello World</h1>
-        {content}
-      </div>
-    </>
-  );
+    // Only 1 Tag is returned by the React
+    return (
+        // This is the react Fragment to return multiple things as a single container
+        <>
+            <Navbar />
+            {/* Nesting Function in the App Component */}
+            <div className="container">
+                <h1>Hello World</h1>
+                {content}
+            </div>
+        </>
+    );
 }
