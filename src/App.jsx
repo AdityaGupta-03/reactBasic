bec6ba07c4d88a32e38252d6d1033d9412360435
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-16 21:45:34 +0530

    Commenting App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+//  To style the app.jsx: App.css is used
 
 function App() {
+  // Only 1 Tag is returned by the React
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coffee with Addy
-        </a>
-      </header>
-    </div>
+    // This is the react Fragment to return multiple things as a single container
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coffee with Addy
+          </a>
+        </header>
+      </div>
+    </>
+    
   );
 }
 
