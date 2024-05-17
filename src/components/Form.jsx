c44263120aa333d62cc726419f3d9ca317e9c6ac
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-17 16:51:28 +0530

    Previewing the data and Word count

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // Changing the state by using hooks
 export default function Form(props) {
-  const [text, setText] = useState("Analyze Below");
+  const [text, setText] = useState("");
 
-  function handleEvent(){
+  function handleUpperCase() {
     setText(text.toUpperCase());
   }
 
-  return (
-    <div className='my-3'>
-        <h1>{props.heading}-{text}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" id="text" rows="8"></textarea>
-        </div>
-        <MyButton onClick={handleEvent} />
-    </div>
-  );
-}
+  function handleLowerCase() {
+    setText(text.toLowerCase());
+  }
 
-function MyButton(props){
-  return(
-    <button className="btn btn-primary" onClick={props.onClick} >Convert to Uppercase</button>
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  }
+
+  // This will handle the spaces tabs and newlines.
+  let content = text === "" ? 0 : text.split(/[\s]+/).length;
+
+  return (
+    <>
+      <div className='my-3'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" onChange={handleOnChange} id="text" rows="8" ></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick={handleUpperCase}>Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleLowerCase}>Convert to Lowercase</button>
+      </div>
+      <hr />
+      
+      <p>{text.length} characters and {content} words</p>
+      <p>{0.004 * content} minutes to read the text</p>
+      <h2>Preview</h2>
+      <p id='preview'>{text}</p>
+    </>
   );
 }
