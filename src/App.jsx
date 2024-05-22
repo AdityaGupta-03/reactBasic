c885b64934374c008aadc5c0b95d17a46496678f
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-22 20:56:00 +0530

    Alert propsmsgs and condition

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,12 @@ export default function App() {
       if(mode==="light"){
         setMode("dark");
         document.body.style.backgroundColor = "black";
+        showAlert("success", "Dark Mode Enabled");
       }
       else{
         setMode("light");
         document.body.style.backgroundColor = "white";
+        showAlert("warning", "light Mode Enabled");
       }
     }
     
