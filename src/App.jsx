4d20d19ee6ee5410e01fe1ff687c477217bcae99
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-28 02:07:52 +0530

    Changing title dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,56 @@ import { useState } from "react";
 
 // Exporting the App Component to the index.js file
 export default function App() {
-    const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
 
-    // Initally we don't want to display the alert
-    const [alert, setAlert] = useState(null);
+  // Initally we don't want to display the alert
+  const [alert, setAlert] = useState(null);
 
-    let showAlert =  (type, word)=>{
-      setAlert({
-        type: type,
-        msg: word
-      })
+  let intervalId;  // Setting the interval ID
 
-      // Dismissing alert after 3 seconds
-      setTimeout(()=>{
-        setAlert(null);
-      },3000);
+  let showAlert = (type, word) => {
+    setAlert({
+      type: type,
+      msg: word
+    })
 
+    // Dismissing alert after 3 seconds
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+
+  }
+
+  let toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(36 26 66)";
+      document.body.style.color = "white";
+      showAlert("success", "Dark Mode Enabled");
+      
+      document.title = `Textutils - Dark Mode`;
+      intervalId = setInterval(() => {
+        document.title = (document.title === "Textutils - Dark Mode") ? "Install this now" : "Textutils - Dark Mode";
+      }, 2000);
+
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("warning", "Dark Mode Disabled");
+      
+      clearInterval(intervalId); // Clear the interval using the stored ID
+      document.title = "Textutils - Light Mode";
     }
+  }
 
-    let toggleMode = ()=>{
-      if(mode==="light"){
-        setMode("dark");
-        document.body.style.backgroundColor = "rgb(36 26 66)";
-        document.body.style.color = "white";
-        showAlert("success", "Dark Mode Enabled");
-      }
-      else{
-        setMode("light");
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-        showAlert("warning", "Dark Mode Disabled");
-      }
-    }
-    
-    return (
-        <>
-          <Navbar title="Coffee With Addy" mode={mode} toggle={toggleMode}/>
-          <Alert alert={alert} />
-          <div className="container">
-            <Form heading="Write Text here" />
-          </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar title="Coffee With Addy" mode={mode} toggle={toggleMode} intervalID={intervalId}/>
+      <Alert alert={alert} />
+      <div className="container">
+        <Form heading="Write Text here" />
+      </div>
+    </>
+  );
 }
