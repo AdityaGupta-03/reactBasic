652bca1bcf47bb6fb86d1947651e4176b204491e
Aditya Gupta <iaditya.gupta03@gmail.com>
2024-05-31 00:01:00 +0530

    Adding About text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,36 +31,36 @@ export default function About() {
             <div className="accordion-item" style={myStyle}>
                 <h2 className="accordion-header">
                     <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
+                        <strong>Analyze your text</strong>
                     </button>
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    Textutility is a software tool that allows users to extract useful insights and information from written text
                     </div>
                 </div>
             </div>
             <div className="accordion-item" style={myStyle}>
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Accordion Item #2
+                        <strong>Free to use</strong>
                     </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    Doesn't require a paid license, fees, or donations, and doesn't have restrictions on how many times you can download it. Examples include Adobe PDF, Yahoo Messenger, and Skype.
                     </div>
                 </div>
             </div>
             <div className="accordion-item" style={myStyle}>
                 <h2 className="accordion-header">
                     <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Accordion Item #3
+                        <strong>Browser Compatibility</strong>
                     </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    Browser Compatibility is the manner in which a web page looks in different web browsers. Different browsers read the website code differently. In other words, Chrome will render a website Differently than FireFox or Internet Explorer will.
                     </div>
                 </div>
             </div>
